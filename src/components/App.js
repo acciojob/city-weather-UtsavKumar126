5fe24331f6589c5d48f74f087a6f8080b0c1377a
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-14 19:59:07 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,62 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useState } from "react";
 
-const App = () => {
+const api = {
+  key: "2e114e8530549cf196ff9e428ae71740",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  /*
+    Search button is pressed. Make a fetch call to the Open Weather Map API.
+  */
+  const searchPressed = (e) => {
+    e.preventDefault();
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <header className="App-header">
+        {/* HEADER  */}
+        <h1>Weather App</h1>
+
+        {/* Search Box - Input + Button  */}
+        <form onSubmit={searchPressed}>
+          <input
+            type="text"
+            placeholder="Enter city/town..."
+            onChange={(e) => setSearch(e.target.value)}
+            className="search"
+          />
+        </form>
+
+        {/* If weather is not undefined display results from API */}
+        {typeof weather.main !== "undefined" ? (
+          <div className="weather">
+            {/* Location  */}
+            <p>{weather.name}</p>
+
+            {/* Temperature Celsius  */}
+            <p>{weather.main.temp}°C</p>
+
+            {/* Condition (Sunny ) */}
+            <p>{weather.weather[0].main}</p>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+          </div>
+        ) : (
+          ""
+        )}
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
